8a8e79542860151f52f1dacbb4f7a287580c82e6
Rafael Winter <rafael.winter@gmail.com>
2013-07-25 15:27:33 -0300

    Create the Factorial Calculator

---
+++ Factorial/FactorialCalculator.h
@@ -0,0 +1,16 @@
+//
+//  FactorialCalculator.h
+//  Factorial
+//
+//  Created by Rafael Winter on 25/07/13.
+//  Copyright (c) 2013 Telefonica | vivo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FactorialCalculator : NSObject
+
+/// Compute the factorial of a number
+- (NSUInteger)factorialOf:(NSUInteger)number;
+
+@end
---
+++ Factorial/FactorialCalculator.m
@@ -0,0 +1,37 @@
+//
+//  FactorialCalculator.m
+//  Factorial
+//
+//  Created by Rafael Winter on 25/07/13.
+//  Copyright (c) 2013 Telefonica | vivo. All rights reserved.
+//
+
+#import "FactorialCalculator.h"
+
+@interface FactorialCalculator ()
+
+- (int)computeFactorial:(NSUInteger)number;
+
+@end
+
+@implementation FactorialCalculator
+
+- (NSUInteger)factorialOf:(NSUInteger)number {
+    return [self computeFactorial:number];
+}
+
+#pragma mark - Class extension
+
+- (int)computeFactorial:(NSUInteger)number {
+    
+    NSUInteger n = number;
+    NSUInteger f = n;
+ 
+    while (n > 1) {
+        f *= --n;
+    }
+    
+    return f;
+}
+
+@end
